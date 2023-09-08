2b1b5d31c452a448b355b191f0fe93108780f5c9
otabek0227 <adrian3806880@gmail.com>
2023-09-08 11:34:33 +0500

    updated

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -12,7 +12,7 @@ export default function PeopleList({ people }) {
                 {people.map(({ id, name, img }) => (
                     <div key={id} className='col-12 col-sm-6 col-md-4 col-lg-3 col-xl'>
                         <div className={styles.list__card}>
-                            <Link to={`star-wars/people/${id}`}>
+                            <Link to={`react-star-wars/people/${id}`}>
                                 <img src={img} alt={name} />
                                 <p>{name}</p>
                             </Link>

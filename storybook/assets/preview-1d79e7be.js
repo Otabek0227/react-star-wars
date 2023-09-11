47d292882a8aca9d6b1b0b1b8850f1ab405df76a
otabek0227 <otabek0227@users.noreply.github.com>
2023-09-11 11:05:14 +0000

    deploy: 744fb94bc465eb4a0ab8d1e4e13f189712bb8af2

--- storybook/assets/preview-1d79e7be.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-d32b5955.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-b78d5408.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-b78d5408.js","./iframe-d32b5955.js","./index-e03f90b5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-09c70a88.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-1d79e7be.js.map

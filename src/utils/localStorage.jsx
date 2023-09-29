2caec293331f13759b133269e7a0a305e5a940c2
otabek0227 <adrian3806880@gmail.com>
2023-09-29 08:43:54 +0500

    Update localStorage.jsx

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -4,6 +4,7 @@ export const getLocalStorage = key => {
     if (data !== null) {
         return JSON.parse(data)
     }
+    return {}
 }
 export const setLocalStorage = (key, data) => {
     localStorage.setItem(key, JSON.stringify(data))
